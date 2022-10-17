d0e04477a4ae4a6a40e084aadc683d44c8e284cf
“xiaoli” <78739803@qq.com>
2022-10-17 22:59:15 +0800

    calendar -1

---
+++ src/types/index.ts
@@ -0,0 +1,6 @@
+export interface IDateType {
+  dataCount: any[]
+  currYear: number
+  currMonth: number
+  currDay: number
+}
--- src/views/main/calendar/calendar.vue
+++ src/views/main/calendar/calendar.vue
@@ -16,21 +16,24 @@
         <li>{{ i.name }}</li>
       </ul>
       <div class="list">
-        <!-- <template v-for="i in state.dataCount" :key="i">
+        <template v-for="i in state.dataCount" :key="i">
           <div class="block">
             <div>
-              <span>{{ i }}</span>
+              <span :class="isCurrDate(i) ? 'active' : ''">{{
+                filterDay(i)
+              }}</span>
             </div>
           </div>
-        </template> -->
+        </template>
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref, reactive } from 'vue'
-
+import { ref, reactive, onMounted } from 'vue'
+import { IDateType } from '@/types'
+import { stat } from 'fs'
 const weeks = reactive([
   { name: '日', id: 0 },
   { name: '一', id: 1 },
@@ -40,12 +43,69 @@ const weeks = reactive([
   { name: '五', id: 5 },
   { name: '六', id: 6 },
 ])
+
+const state = reactive<IDateType>({
+  dataCount: [],
+  currYear: 0,
+  currMonth: 0,
+  currDay: 0,
+})
+
+const currYear = new Date().getFullYear()
+const currMonth = new Date().getMonth()
+const currDay = new Date().getDay()
+
+onMounted(() => {})
+const getDateCount = () => {}
+
+// 日期大于 0  的显示
+const filterDay = (day: number) => {
+  return day > 0 ? day : 0
+}
+
+// 当前日期， 当前年月
+const isCurrDate = (date: number) => {
+  if (
+    date == state.currDay &&
+    state.currYear == currYear &&
+    state.currMonth == currMonth
+  ) {
+    return true
+  } else {
+    return false
+  }
+}
 </script>
 
 <style lang="less" scoped>
+.calendar {
+  padding: 20px;
+  width: 600px;
+  height: 400px;
+}
 .head {
   width: 500px;
   display: flex;
-  align-item: center;
+  align-items: center;
+}
+
+.head {
+  padding: 20px;
+  .week {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+}
+
+.active {
+  background-color: red;
+  color: #fff;
+  width: 26px;
+  height: 26px;
+  border-radius: 50%;
+  font-size: 13px;
+  text-align: center;
+  line-height: 26px;
 }
 </style>
